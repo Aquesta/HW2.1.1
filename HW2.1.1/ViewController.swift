7e750f1503dd454356a44cddb2ca6cb677ec36ea
Aquesta <gkrasnoshlyk@gmail.com>
2020-08-19 20:35:34 +0500

    Initial Commit

---
+++ HW2.1.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.1.1
+//
+//  Created by Геннадий Красношлык on 19.08.2020.
+//  Copyright © 2020 Aquesta's. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
